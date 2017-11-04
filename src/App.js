b6c14fa3032eba3c1b1e8b607657068ef16611b2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 16:43:56 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ export class App extends Component {
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {
-
-        }
+        <QuoteForm />
+        <Quotes />
       </div>
     );
   }
