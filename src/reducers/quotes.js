e29c8e4f951f960aa77881474f00c731d25229c5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 16:27:35 +0000

    automatically backed up by learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,18 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case: 'ADD_QUOTE':
+      return [].concat(state, action.quote)
+    case: 'REMOVE_QUOTE':
+      let idx = state.indexOf(action.quoteId)
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+    case: 'UPVOTE_QUOTE':
+      let upvote = state.find.(quote => quote.id === action.quoteId)
+      return [Object.assign({}, upvote, { votes: ++upvote.votes })]
+    case: 'DOWNVOTE_QUOTE':
+      let downvote = state.find.(quote => quote.id === action.quoteId)
+      let downvoting = (downvote.votes > 0) ? --downvote.votes : 0
+      return [Object.assign({}, downvote, {votes: downvoting})]
+    default: 
+      return state 
+  }
 }
