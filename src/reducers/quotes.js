bde7e7b6c58518b6424b7777218d0af1a6f6fcb5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 16:28:20 +0000

    automatically backed up by learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,14 +1,14 @@
 export default (state = [], action) => {
   switch (action.type) {
-    case: 'ADD_QUOTE':
+    case 'ADD_QUOTE':
       return [].concat(state, action.quote)
-    case: 'REMOVE_QUOTE':
+    case 'REMOVE_QUOTE':
       let idx = state.indexOf(action.quoteId)
       return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
-    case: 'UPVOTE_QUOTE':
+    case 'UPVOTE_QUOTE':
       let upvote = state.find.(quote => quote.id === action.quoteId)
       return [Object.assign({}, upvote, { votes: ++upvote.votes })]
-    case: 'DOWNVOTE_QUOTE':
+    case 'DOWNVOTE_QUOTE':
       let downvote = state.find.(quote => quote.id === action.quoteId)
       let downvoting = (downvote.votes > 0) ? --downvote.votes : 0
       return [Object.assign({}, downvote, {votes: downvoting})]
